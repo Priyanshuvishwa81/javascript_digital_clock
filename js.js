29829701de3d7872fbda408a302686ef70633177
priyanshu <100010630+Priyanshuvishwa81@users.noreply.github.com>
2023-02-04 19:03:58 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,182 @@
+const h1_one = [8, 12, 13, 18, 23, 27, 28, 29];
+const h1_two = [7, 8, 9, 14, 19, 18, 17, 22, 27, 28, 29];
+const h1_three = [7, 8, 9, 19, 14, 18, 17, 24, 29, 28, 27];
+const h1_four = [7, 12, 17, 18, 19, 14, 9, 24, 29];
+const h1_five = [7, 8, 9, 19, 18, 17, 27, 28, 29, 12, 24];
+const h1_six = [7, 8, 9, 19, 18, 17, 22, 27, 28, 29, 12, 24];
+const h1_seven = [7, 8, 9, 14, 19, 29, 24];
+const h1_eight = [7, 8, 9, 14, 19, 18, 17, 22, 27, 28, 29, 12, 24];
+const h1_nine = [7, 8, 9, 14, 19, 18, 17, 27, 28, 29, 12, 24];
+const h1_zero = [7, 8, 9, 14, 19, 17, 22, 27, 28, 29, 12, 24];
+
+
+const h2_one = [43, 47, 53, 58, 48, 62, 63, 64];
+const h2_two = [42, 43, 44, 49, 52, 53, 54, 57, 62, 63, 64];
+const h2_three = [42, 43, 44, 49, 52, 53, 54, 59, 62, 63, 64];
+const h2_four = [42, 44, 47, 49, 52, 53, 54, 59, 64];
+const h2_five = [42, 43, 44, 47, 52, 53, 54, 59, 62, 63, 64];
+const h2_six = [42, 43, 44, 47, 52, 53, 54, 57, 59, 62, 63, 64];
+const h2_seven = [42, 43, 44, 49, 54, 59, 64];
+const h2_eight = [42, 43, 44, 47, 49, 52, 53, 54, 57, 59, 62, 63, 64];
+const h2_nine = [42, 43, 44, 47, 49, 52, 53, 54, 59, 62, 63, 64];
+const h2_zero = [42, 43, 44, 47, 49, 52, 54, 57, 59, 62, 63, 64];
+
+
+const h3_one = [78, 82, 83, 88, 93, 97, 98, 99];
+const h3_two = [77, 78, 79, 84, 89, 88, 87, 92, 97, 98, 99];
+const h3_three = [77, 78, 79, 89, 84, 88, 87, 94, 99, 98, 97];
+const h3_four = [77, 82, 87, 88, 89, 84, 79, 94, 99];
+const h3_five = [77, 78, 79, 89, 88, 87, 97, 98, 99, 82, 94];
+const h3_six = [77, 78, 79, 89, 88, 87, 92, 97, 98, 99, 82, 94];
+const h3_seven = [77, 78, 79, 84, 89, 99, 94];
+const h3_eight = [77, 78, 79, 84, 89, 88, 87, 92, 97, 98, 99, 82, 94];
+const h3_nine = [77, 78, 79, 84, 89, 88, 87, 97, 98, 99, 82, 94];
+const h3_zero = [77, 78, 79, 84, 89, 87, 92, 97, 98, 99, 82, 94];
+
+
+
+const h4_one = [113, 117, 123, 128, 118, 132, 133, 134];
+const h4_two = [112, 113, 114, 119, 122, 123, 124, 127, 132, 133, 134];
+const h4_three = [112, 113, 114, 119, 122, 123, 124, 129, 132, 133, 134];
+const h4_four = [112, 114, 117, 119, 122, 123, 124, 129, 134];
+const h4_five = [112, 113, 114, 117, 122, 123, 124, 129, 132, 133, 134];
+const h4_six = [112, 113, 114, 117, 122, 123, 124, 127, 129, 132, 133, 134];
+const h4_seven = [112, 113, 114, 119, 124, 129, 134];
+const h4_eight = [112, 113, 114, 117, 119, 122, 123, 124, 127, 129, 132, 133, 134];
+const h4_nine = [112, 113, 114, 117, 119, 122, 123, 124, 129, 132, 133, 134];
+const h4_zero = [112, 113, 114, 117, 119, 122, 124, 127, 129, 132, 133, 134];
+
+
+const h5_one = [148, 152, 153, 158, 163, 167, 168, 169];
+const h5_two = [147, 148, 149, 154, 159, 158, 157, 162, 167, 168, 169];
+const h5_three = [147, 148, 149, 159, 154, 158, 157, 164, 169, 168, 167];
+const h5_four = [147, 152, 157, 158, 159, 154, 149, 164, 169];
+const h5_five = [147, 148, 149, 159, 158, 157, 167, 168, 169, 152, 164];
+const h5_six = [147, 148, 149, 159, 158, 157, 162, 167, 168, 69, 152, 164];
+const h5_seven = [147, 148, 159, 154, 149, 169, 164];
+const h5_eight = [147, 148, 149, 154, 159, 158, 157, 162, 167, 168, 169, 152, 164];
+const h5_nine = [147, 148, 149, 154, 159, 158, 157, 167, 168, 169, 152, 164];
+const h5_zero = [147, 148, 149, 154, 159, 157, 162, 167, 168, 169, 152, 164];
+
+
+
+const h6_one = [183, 187, 193, 198, 188, 202, 203, 204];
+const h6_two = [182, 183, 184, 189, 192, 193, 194, 197, 202, 203, 204];
+const h6_three = [182, 183, 184, 189, 192, 193, 194, 199, 202, 203, 204];
+const h6_four = [182, 184, 187, 189, 192, 193, 194, 199, 204];
+const h6_five = [182, 183, 184, 187, 192, 193, 194, 199, 202, 203, 204];
+const h6_six = [182, 183, 184, 187, 192, 193, 194, 197, 199, 202, 203, 204];
+const h6_seven = [182, 183, 184, 189, 194, 199, 204];
+const h6_eight = [182, 183, 184, 187, 189, 192, 193, 194, 197, 199, 202, 203, 204];
+const h6_nine = [182, 183, 184, 187, 189, 192, 193, 194, 199, 202, 203, 204];
+const h6_zero = [182, 183, 184, 187, 189, 192, 194, 197, 199, 202, 203, 204];
+
+let runs_clear = (range1, range2) => {
+
+    for (let i = range1; i <= range2; i++) {
+        let data = document.getElementById(i);
+        data.style.backgroundColor = 'transparent';
+    }
+}
+
+
+let run = (number) => {
+
+    for (let i = 0; i <= number.length - 1; i++) {
+        let data = document.getElementById(number[i]);
+        data.style.backgroundColor = 'green';
+    }
+
+}
+
+let blink_helper = true;
+
+let move = () => {
+
+
+    const all_num1 = [h1_zero, h1_one, h1_two, h1_three, h1_four, h1_five, h1_six, h1_seven, h1_eight, h1_nine];
+    const all_num2 = [h2_zero, h2_one, h2_two, h2_three, h2_four, h2_five, h2_six, h2_seven, h2_eight, h2_nine];
+
+    var date = new Date();
+    let my_hour = date.getHours().toString();
+    // alert(my_hour);
+
+    let my_hour_new_arr = my_hour.split("");
+    runs_clear(1, 35);
+    run(all_num1[my_hour_new_arr[0]]);
+    runs_clear(36, 70);
+    run(all_num2[my_hour_new_arr[1]]);
+
+
+
+
+    // second minit
+
+    const all_num3 = [h3_zero, h3_one, h3_two, h3_three, h3_four, h3_five, h3_six, h3_seven, h3_eight, h3_nine];
+    const all_num4 = [h4_zero, h4_one, h4_two, h4_three, h4_four, h4_five, h4_six, h4_seven, h4_eight, h4_nine];
+
+    let my_min = date.getMinutes().toString();
+
+    if (my_min.length == 1) {
+        my_min = "0" + my_min;
+    }
+    let my_min_new_arr = my_min.split("");
+
+
+    runs_clear(71, 105);
+
+    run(all_num3[my_min_new_arr[0]]);
+    runs_clear(106, 140);
+    run(all_num4[my_min_new_arr[1]]);
+
+
+    // third
+
+    const all_num5 = [h5_zero, h5_one, h5_two, h5_three, h5_four, h5_five, h5_six, h5_seven, h5_eight, h5_nine];
+    const all_num6 = [h6_zero, h6_one, h6_two, h6_three, h6_four, h6_five, h6_six, h6_seven, h6_eight, h6_nine];
+
+    let my_sec = date.getSeconds().toString();
+    if (my_sec.length == 1) {
+        my_sec = "0" + my_sec;
+    }
+
+    let my_sec_new_arr = my_sec.split("");
+
+    // alert(my_sec_new_arr);
+
+    runs_clear(141, 175);
+
+    run(all_num5[my_sec_new_arr[0]]);
+    runs_clear(176, 210);
+    run(all_num6[my_sec_new_arr[1]]);
+
+   let  dot_arr = ["dot1","dot2","dot3","dot4"];
+
+
+    let blink = () => {
+        if (blink_helper == true) {
+            for (let i = 0; i <= dot_arr.length-1; i++) {
+                let light = document.getElementById(dot_arr[i]);
+                light.style.display='none';
+                blink_helper = false;
+            }
+
+        }
+        else if (blink_helper == false) {
+            for (let i = 0; i <= dot_arr.length-1; i++) {
+                let light = document.getElementById(dot_arr[i]);
+                light.style.display='block';
+                blink_helper = true;
+            }
+        }
+
+    }
+
+    blink();
+
+
+}
+
+setInterval(move, 1000);
+
+
