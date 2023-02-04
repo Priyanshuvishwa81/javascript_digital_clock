9bfe0e99a887182b6480544a844092a6d0cb8fed
priyanshu <100010630+Priyanshuvishwa81@users.noreply.github.com>
2023-02-04 19:52:21 +0530

    Update js.js

--- js.js
+++ js.js
@@ -90,82 +90,89 @@ let run = (number) => {
 }
 
 let blink_helper = true;
+const all_num1 = [h1_zero, h1_one, h1_two, h1_three, h1_four, h1_five, h1_six, h1_seven, h1_eight, h1_nine];
+const all_num2 = [h2_zero, h2_one, h2_two, h2_three, h2_four, h2_five, h2_six, h2_seven, h2_eight, h2_nine];
+
+const all_num3 = [h3_zero, h3_one, h3_two, h3_three, h3_four, h3_five, h3_six, h3_seven, h3_eight, h3_nine];
+const all_num4 = [h4_zero, h4_one, h4_two, h4_three, h4_four, h4_five, h4_six, h4_seven, h4_eight, h4_nine];
+
+const all_num5 = [h5_zero, h5_one, h5_two, h5_three, h5_four, h5_five, h5_six, h5_seven, h5_eight, h5_nine];
+const all_num6 = [h6_zero, h6_one, h6_two, h6_three, h6_four, h6_five, h6_six, h6_seven, h6_eight, h6_nine];
+
 
 let move = () => {
 
 
-    const all_num1 = [h1_zero, h1_one, h1_two, h1_three, h1_four, h1_five, h1_six, h1_seven, h1_eight, h1_nine];
-    const all_num2 = [h2_zero, h2_one, h2_two, h2_three, h2_four, h2_five, h2_six, h2_seven, h2_eight, h2_nine];
 
     var date = new Date();
     let my_hour = date.getHours().toString();
-    // alert(my_hour);
+    let my_min = date.getMinutes().toString();
+    let my_sec = date.getSeconds().toString();
 
+    
+    // first hour
+
+
+    if (my_min == "0") {
+        runs_clear(1, 35);
+        runs_clear(36, 70);
+    }
+    if (my_hour.length == 1) {
+        my_hour = "0" + my_min;
+    }
     let my_hour_new_arr = my_hour.split("");
-    runs_clear(1, 35);
+    
     run(all_num1[my_hour_new_arr[0]]);
-    runs_clear(36, 70);
     run(all_num2[my_hour_new_arr[1]]);
 
-
-
-
     // second minit
-
-    const all_num3 = [h3_zero, h3_one, h3_two, h3_three, h3_four, h3_five, h3_six, h3_seven, h3_eight, h3_nine];
-    const all_num4 = [h4_zero, h4_one, h4_two, h4_three, h4_four, h4_five, h4_six, h4_seven, h4_eight, h4_nine];
-
-    let my_min = date.getMinutes().toString();
-
+    
+    if (my_sec == "0") {
+        runs_clear(71, 105);
+        runs_clear(106, 140);
+    }
     if (my_min.length == 1) {
         my_min = "0" + my_min;
     }
     let my_min_new_arr = my_min.split("");
 
+    
 
-    runs_clear(71, 105);
 
     run(all_num3[my_min_new_arr[0]]);
-    runs_clear(106, 140);
     run(all_num4[my_min_new_arr[1]]);
 
 
     // third
-
-    const all_num5 = [h5_zero, h5_one, h5_two, h5_three, h5_four, h5_five, h5_six, h5_seven, h5_eight, h5_nine];
-    const all_num6 = [h6_zero, h6_one, h6_two, h6_three, h6_four, h6_five, h6_six, h6_seven, h6_eight, h6_nine];
-
-    let my_sec = date.getSeconds().toString();
+    
     if (my_sec.length == 1) {
-        my_sec = "0" + my_sec;
+        my_sec = "0"+ my_sec;
     }
-
     let my_sec_new_arr = my_sec.split("");
 
-    // alert(my_sec_new_arr);
 
     runs_clear(141, 175);
+    runs_clear(176, 210);
 
     run(all_num5[my_sec_new_arr[0]]);
-    runs_clear(176, 210);
     run(all_num6[my_sec_new_arr[1]]);
 
-   let  dot_arr = ["dot1","dot2","dot3","dot4"];
+    let dot_arr = ["dot1", "dot2", "dot3", "dot4"];
 
 
     let blink = () => {
         if (blink_helper == true) {
-            for (let i = 0; i <= dot_arr.length-1; i++) {
+            for (let i = 0; i <= dot_arr.length - 1; i++) {
                 let light = document.getElementById(dot_arr[i]);
-                light.style.display='none';
+                light.style.display = 'none';
                 blink_helper = false;
             }
 
         }
         else if (blink_helper == false) {
-            for (let i = 0; i <= dot_arr.length-1; i++) {
+            for (let i = 0; i <= dot_arr.length - 1; i++) {
                 let light = document.getElementById(dot_arr[i]);
-                light.style.display='block';
+                light.style.display = 'block';
                 blink_helper = true;
             }
         }
